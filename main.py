1dc57fea922e81c0e10c628cc57f7b9588dea8e2
11KK12 <76783163+11KK12@users.noreply.github.com>
2024-05-07 09:15:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,9 @@ def run():
   start_button_disabled = True
 
   if uploaded_file is not None:
-    
-      with st.spinner("Checking your input..."):
+
+      spinner_message = "Checking your input..."
+      with st.spinner(spinner_message):
 
         #### 1. Read PDF documents with PyPDF to find starts of templates
         template_list = find_templates_in_pdf(uploaded_file)
@@ -40,77 +41,80 @@ def run():
             st.markdown("\nEstimated cost for extraction and validation is {:0.2f} €.\n".format(estimated_costs))
             start_button_disabled = False
             
-      if st.button("Start", type="primary", use_container_width=True, disabled=start_button_disabled):
+          if st.button("Start", type="primary", use_container_width=True, disabled=start_button_disabled):
 
-          # TODO hide button after click
-          text_placeholder.empty()
-          start_button_disabled = True
+              spinner_message = "Extracting data from file..."
+    
+              # TODO hide button after click
+              text_placeholder.empty()
+              start_button_disabled = True
+    
+              # Create dataframe to store extraction results
+              template_fields = pd.DataFrame()
+    
+              # Set up Azure AI Document Intelligence
+              try:
+                  document_ai_endpoint = st.secrets["DOCUMENT_AI_ENDPOINT"]
+                  document_ai_key = st.secrets["DOCUMENT_AI_KEY"]
+              except:
+                  document_ai_endpoint = os.environ["DOCUMENT_AI_ENDPOINT"]
+                  document_ai_key = os.environ["DOCUMENT_AI_KEY"]
+    
+              document_analysis_client = DocumentAnalysisClient(
+                  endpoint=document_ai_endpoint, credential=AzureKeyCredential(document_ai_key)
+              )
+    
+              # Generate embeddings of question variables for labelling of extracted paragraphs
+              question_variables = generate_question_embeddings()
+    
+              extraction_bar = st.progress(0, text="Extracting data...")
+    
+              # Extract data from template
+              for i, template in enumerate(template_list[:1]): # TODO [:1] for test purposes, remove later
+    
+                  # Show progress
+                  extraction_bar.progress(((i+1)/template_count), text="Extracting data from " + template["f_product_name"] + "...")
+    
+                  template_data = extract_template_data(template, uploaded_file, document_analysis_client, question_variables)
+    
+                  #Save all results from this template to dataframe
+                  id = template_data["f_legal_entity_identifier"]
+                  for k,v in template_data.items():
+                      if type(v) == dict: # -> q_n_a_pairs
+                          key = v["label"]
+                          try: 
+                              # there might be multiple answers to one label
+                              existing_value = template_fields.at[key, id]
+                              template_fields.at[key, id] = existing_value + " / " + v["answer"]
+                          except:
+                              template_fields.at[key, id] = v["answer"]
+                      else:
+                          template_fields.at[k, id] = v
 
-          # Create dataframe to store extraction results
-          template_fields = pd.DataFrame()
-
-          # Set up Azure AI Document Intelligence
-          try:
-              document_ai_endpoint = st.secrets["DOCUMENT_AI_ENDPOINT"]
-              document_ai_key = st.secrets["DOCUMENT_AI_KEY"]
-          except:
-              document_ai_endpoint = os.environ["DOCUMENT_AI_ENDPOINT"]
-              document_ai_key = os.environ["DOCUMENT_AI_KEY"]
-
-          document_analysis_client = DocumentAnalysisClient(
-              endpoint=document_ai_endpoint, credential=AzureKeyCredential(document_ai_key)
-          )
-
-          # Generate embeddings of question variables for labelling of extracted paragraphs
-          question_variables = generate_question_embeddings()
-
-          extraction_bar = st.progress(0, text="Extracting data...")
-
-          # Extract data from template
-          for i, template in enumerate(template_list[:2]): # TODO :3 for test purposes
-
-              # Show progress
-              extraction_bar.progress(((i+1)/template_count), text="Extracting data from " + template["f_product_name"] + "...")
-
-              template_data = extract_template_data(template, uploaded_file, document_analysis_client, question_variables)
-
-              #Save all results from this template to dataframe
-              id = template_data["f_legal_entity_identifier"]
-              for k,v in template_data.items():
-                  if type(v) == dict: # -> q_n_a_pairs
-                      key = v["label"]
-                      try: 
-                          # there might be multiple answers to one label
-                          existing_value = template_fields.at[key, id]
-                          template_fields.at[key, id] = existing_value + " / " + v["answer"]
-                      except:
-                          template_fields.at[key, id] = v["answer"]
-                  else:
-                      template_fields.at[k, id] = v
-
-          validation_bar = st.progress(0, text="Validating data...")
-
-          template_checks = {}
-
-          # Validate extracted data from each template
-          tempys = template_fields.transpose()
-
-          for i, index in enumerate(tempys.index):
-              validation_bar.progress(((i+1)/len(tempys.index)), text="Validating data from " + tempys.at[index, "f_product_name"] + "...")
-              id = tempys.at[index,"f_legal_entity_identifier"]
-              validation_results = validate(tempys, index)
-              template_checks[id] = validation_results
-
-          output = template_checks_to_excel(tempys, template_checks) # is ouput still iobytes object? needs to be converted to excel?
-          st.ballons()
-          st.download_button(
-              label="Download validation results",
-              data=output,
-              #file_name="validation_results.xlsx",
-              type="primary",
-              use_container_width=True,
-              mime = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-          )
+              spinner_message = "Validating extracted data..."
+              validation_bar = st.progress(0, text="Validating data...")
+    
+              template_checks = {}
+    
+              # Validate extracted data from each template
+              tempys = template_fields.transpose()
+    
+              for i, index in enumerate(tempys.index):
+                  validation_bar.progress(((i+1)/len(tempys.index)), text="Validating data from " + tempys.at[index, "f_product_name"] + "...")
+                  id = tempys.at[index,"f_legal_entity_identifier"]
+                  validation_results = validate(tempys, index)
+                  template_checks[id] = validation_results
+    
+              output = template_checks_to_excel(tempys, template_checks) # is ouput still iobytes object? needs to be converted to excel?
+              st.balloons()
+              st.download_button(
+                  label="Download validation results",
+                  data=output,
+                  #file_name="validation_results.xlsx",
+                  type="primary",
+                  use_container_width=True,
+                  mime = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+              )
 
 if __name__ == "__main__":
     run()
