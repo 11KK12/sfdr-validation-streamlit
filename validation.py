46c87e613ba2b7ff4c2fa3b4252168e16e1a37bd
11KK12 <76783163+11KK12@users.noreply.github.com>
2024-05-20 15:27:39 +0300

    Update validation.py

--- validation.py
+++ validation.py
@@ -111,7 +111,7 @@ def validate(template_fields, i):
         comment = "Product promotes environmental features and 'No significant harm' statement has not been included."
 
         relevant_text = a_no_significant_harm + a_alignment_with_OECD_guidelines
-        do_not_harm_statement_excerpt = "EU:n luokitusjärjestelmässä vahvistetaan "ei merkittävää haittaa" -periaate"
+        do_not_harm_statement_excerpt = 'EU:n luokitusjärjestelmässä vahvistetaan "ei merkittävää haittaa" -periaate'
 
         if do_not_harm_statement_excerpt in relevant text:
             value = True
